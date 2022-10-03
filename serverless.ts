eb49ce28ba5f7117da4a2f8e231a74b51562aed1
Davi Carvalho <davircsp@hotmail.com>
2022-10-03 13:16:26 -0300

    pdf error fixed

--- serverless.ts
+++ serverless.ts
@@ -52,12 +52,14 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk, chrome-aws-lambda'],
+      exclude: ['aws-sdk'],
       target: 'node16',
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
-
+      external: [
+        'chrome-aws-lambda'
+      ]
     },
     dynamodb: {
       stages: ['dev', 'local'],
